11b11246555fb30fed57294319e30fd71fe1d6f5
RyzenMu <muruga13in2022@gmail.com>
2025-03-13 05:33:47 +0530

    first commit

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
 // verifying JWT on the server
 
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 // secret key used to verify the token
-const secretKey = 'earth';
+const secretKey = "earth";
 
-// create a jwt token 
-const {createJwt} = require('./webpage');
+// create a jwt token
+const { createJwt } = require("./webpage");
 const token = createJwt();
 
-
 // Verify the token
-jwt.verify(token, secretKey, (err, decoded)=> {
-    if(err) {
-        console.log('Token verification failed', err);        
-    } else {
-        console.log('Decoded token', decoded);        
-    }
-})
+jwt.verify(token, secretKey, (err, decoded) => {
+  if (err) {
+    console.log("Token verification failed", err);
+  } else {
+    console.log("Decoded token", decoded);
+  }
+});
+
+const express = require("express");
+
+const app = express();
+
+app.post("/", function (req, res) {
+  res.send("Say hello");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`server started`);
+});
